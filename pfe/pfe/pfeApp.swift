df693af0c73746ceedf48b5cf0fe1ab147c6c387
Bennourhene19 <127225311+Bennourhene19@users.noreply.github.com>
2024-05-04 15:30:06 +0100

    Add files via upload

---
+++ pfe/pfe/pfeApp.swift
@@ -0,0 +1,17 @@
+//
+//  pfeApp.swift
+//  pfe
+//
+//  Created by Nourhaine on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct pfeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
